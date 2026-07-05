3ecd59ef3b3b4e41faa5ded246d90c38f3dfe3ea
wish86 <mail.chromaky@gmail.com>
2026-07-05 19:05:03 +1000

    Created basic draggable window.

--- WindowsColorblindHelperUI/Form1.Designer.cs
+++ WindowsColorblindHelperUI/Form1.Designer.cs
@@ -28,38 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.titlebar = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // button1
+            // titlebar
             // 
-            this.button1.Location = new System.Drawing.Point(162, 83);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.titlebar.BackColor = System.Drawing.Color.Orange;
+            this.titlebar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.titlebar.Location = new System.Drawing.Point(5, 5);
+            this.titlebar.Name = "titlebar";
+            this.titlebar.Size = new System.Drawing.Size(269, 33);
+            this.titlebar.TabIndex = 1;
+            this.titlebar.Paint += new System.Windows.Forms.PaintEventHandler(this.titlebar_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(5, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(269, 89);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
             this.ClientSize = new System.Drawing.Size(279, 132);
             this.ControlBox = false;
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.titlebar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(5);
             this.Text = "Form1";
             this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel titlebar;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- WindowsColorblindHelperUI/Form1.cs
+++ WindowsColorblindHelperUI/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,14 +13,58 @@ namespace WindowsColorblindHelperUI
 {
     public partial class Form1 : Form
     {
+        // For dragging custom titlebar. I trust this I guess.
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+
         public Form1()
         {
             InitializeComponent();
 
             this.TopMost = true;
+
+            this.titlebar.MouseDown += new MouseEventHandler(panelTitleBar_MouseDown);
+
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void panelTitleBar_MouseDown(object sender, MouseEventArgs e)
+        {  
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    
+
+        // Shadow!!!
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int CS_DROPSHADOW = 0x20000;
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                return cp;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void titlebar_Paint(object sender, PaintEventArgs e)
         {
 
         }
