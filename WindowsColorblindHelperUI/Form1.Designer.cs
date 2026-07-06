1bd624f710d45704e267b9f4a2f908d8eb492351
wish86 <mail.chromaky@gmail.com>
2026-07-06 20:07:31 +1000

    Buttons and main logic done. Changing graphics and cleaning up code..

--- WindowsColorblindHelperUI/Form1.Designer.cs
+++ WindowsColorblindHelperUI/Form1.Designer.cs
@@ -32,7 +32,11 @@
             this.minbutton = new System.Windows.Forms.Button();
             this.closebutton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.Protan = new System.Windows.Forms.Button();
+            this.Grayscale = new System.Windows.Forms.Button();
+            this.ToggleFilter = new System.Windows.Forms.Button();
             this.titlebar.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // titlebar
@@ -41,56 +45,105 @@
             this.titlebar.Controls.Add(this.minbutton);
             this.titlebar.Controls.Add(this.closebutton);
             this.titlebar.Dock = System.Windows.Forms.DockStyle.Top;
-            this.titlebar.Location = new System.Drawing.Point(5, 5);
+            this.titlebar.Location = new System.Drawing.Point(4, 4);
+            this.titlebar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.titlebar.Name = "titlebar";
-            this.titlebar.Size = new System.Drawing.Size(269, 33);
+            this.titlebar.Size = new System.Drawing.Size(201, 27);
             this.titlebar.TabIndex = 1;
             // 
             // minbutton
             // 
-            this.minbutton.Location = new System.Drawing.Point(170, 4);
+            this.minbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.minbutton.FlatAppearance.BorderSize = 0;
+            this.minbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minbutton.Image = global::WindowsColorblindHelperUI.Properties.Resources.app_min;
+            this.minbutton.Location = new System.Drawing.Point(148, 0);
+            this.minbutton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.minbutton.Name = "minbutton";
-            this.minbutton.Size = new System.Drawing.Size(36, 23);
+            this.minbutton.Size = new System.Drawing.Size(27, 27);
             this.minbutton.TabIndex = 1;
-            this.minbutton.Text = "button1";
             this.minbutton.UseVisualStyleBackColor = true;
             this.minbutton.Click += new System.EventHandler(this.minbutton_Click);
             // 
             // closebutton
             // 
-            this.closebutton.Location = new System.Drawing.Point(220, 4);
+            this.closebutton.FlatAppearance.BorderSize = 0;
+            this.closebutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closebutton.ForeColor = System.Drawing.Color.White;
+            this.closebutton.Image = global::WindowsColorblindHelperUI.Properties.Resources.app_x;
+            this.closebutton.Location = new System.Drawing.Point(175, 0);
+            this.closebutton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.closebutton.Name = "closebutton";
-            this.closebutton.Size = new System.Drawing.Size(38, 23);
+            this.closebutton.Size = new System.Drawing.Size(27, 27);
             this.closebutton.TabIndex = 0;
-            this.closebutton.Text = "button1";
             this.closebutton.UseVisualStyleBackColor = true;
             this.closebutton.Click += new System.EventHandler(this.closebutton_Click);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.Protan);
+            this.panel1.Controls.Add(this.Grayscale);
+            this.panel1.Controls.Add(this.ToggleFilter);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(5, 38);
+            this.panel1.Location = new System.Drawing.Point(4, 31);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(269, 89);
+            this.panel1.Size = new System.Drawing.Size(201, 72);
             this.panel1.TabIndex = 2;
+            // 
+            // Protan
+            // 
+            this.Protan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Protan.Image = global::WindowsColorblindHelperUI.Properties.Resources.off;
+            this.Protan.Location = new System.Drawing.Point(137, 5);
+            this.Protan.Name = "Protan";
+            this.Protan.Size = new System.Drawing.Size(60, 60);
+            this.Protan.TabIndex = 2;
+            this.Protan.UseVisualStyleBackColor = true;
+            this.Protan.Click += new System.EventHandler(this.Protan_Click);
+            // 
+            // Grayscale
+            // 
+            this.Grayscale.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Grayscale.Image = global::WindowsColorblindHelperUI.Properties.Resources.off;
+            this.Grayscale.Location = new System.Drawing.Point(71, 5);
+            this.Grayscale.Name = "Grayscale";
+            this.Grayscale.Size = new System.Drawing.Size(60, 60);
+            this.Grayscale.TabIndex = 1;
+            this.Grayscale.UseVisualStyleBackColor = true;
+            this.Grayscale.Click += new System.EventHandler(this.Grayscale_Click);
+            // 
+            // ToggleFilter
+            // 
+            this.ToggleFilter.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ToggleFilter.Image = global::WindowsColorblindHelperUI.Properties.Resources.off;
+            this.ToggleFilter.Location = new System.Drawing.Point(5, 5);
+            this.ToggleFilter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ToggleFilter.Name = "ToggleFilter";
+            this.ToggleFilter.Size = new System.Drawing.Size(60, 60);
+            this.ToggleFilter.TabIndex = 0;
+            this.ToggleFilter.UseVisualStyleBackColor = true;
+            this.ToggleFilter.Click += new System.EventHandler(this.ToggleFilter_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Orange;
-            this.ClientSize = new System.Drawing.Size(279, 132);
+            this.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.ClientSize = new System.Drawing.Size(209, 107);
             this.ControlBox = false;
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.titlebar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Text = "Form1";
             this.TopMost = true;
             this.titlebar.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -100,6 +153,9 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button minbutton;
         private System.Windows.Forms.Button closebutton;
+        private System.Windows.Forms.Button ToggleFilter;
+        private System.Windows.Forms.Button Grayscale;
+        private System.Windows.Forms.Button Protan;
     }
 }
 
--- WindowsColorblindHelperUI/Form1.cs
+++ WindowsColorblindHelperUI/Form1.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace WindowsColorblindHelperUI
 {
@@ -21,26 +23,14 @@ namespace WindowsColorblindHelperUI
         private const int WM_NCLBUTTONDOWN = 0xA1;
         private const int HT_CAPTION = 0x2;
 
-        public Form1()
-        {
-            InitializeComponent();
-
-            this.TopMost = true;
-
-            this.titlebar.MouseDown += new MouseEventHandler(panelTitleBar_MouseDown);
-
-
-        }
-
         private void panelTitleBar_MouseDown(object sender, MouseEventArgs e)
-        {  
+        {
             if (e.Button == MouseButtons.Left)
             {
                 ReleaseCapture();
                 SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
             }
         }
-    
 
         // Shadow!!!
         protected override CreateParams CreateParams
@@ -56,12 +46,240 @@ namespace WindowsColorblindHelperUI
 
         private void closebutton_Click(object sender, EventArgs e)
         {
-            this.Close();  
+            this.Close();
         }
 
         private void minbutton_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+
+        private bool GetColorFilterActiveValue()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\ColorFiltering"))
+            {
+                if (key != null)
+                {
+                    object value = key.GetValue("Active");
+                    if (value != null)
+                    {
+                        return Convert.ToInt32(value) == 1;
+                    }
+                }
+            }
+            return false; // key or value doesn't exist → treat as inactive
+        }
+
+        private int? GetColorFilterTypeValue()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\ColorFiltering"))
+            {
+                if (key != null)
+                {
+                    object value = key.GetValue("FilterType");
+                    if (value != null)
+                    {
+                        return Convert.ToInt32(value);
+                    }
+                }
+            }
+            return null; // key or value doesn't exist
+        }
+
+        bool isToggled = false;
+        bool isGrayScale = false;
+        bool isProtan = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.TopMost = true;
+            this.titlebar.MouseDown += new MouseEventHandler(panelTitleBar_MouseDown);
+
+            if (GetColorFilterActiveValue())
+            {
+                // Debug.WriteLine("Your debug message here");
+                isToggled = true;
+                ToggleFilter.Image = Properties.Resources.on1;
+            }
+
+            if(GetColorFilterTypeValue() == 0)
+            {
+                // Debug.WriteLine("grey");
+                isGrayScale = true;
+                Grayscale.Image = Properties.Resources.on1;
+            }
+
+            if (GetColorFilterTypeValue() == 4)
+            {
+                // Debug.WriteLine("protan");
+                Protan.Image = Properties.Resources.on1;
+                isProtan = true;
+            }
+
+
+        }
+
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+        private const byte VK_LWIN = 0x5B;
+        private const byte VK_CONTROL = 0x11;
+        private const byte VK_C = 0x43;
+        private const uint KEYEVENTF_KEYUP = 0x0002;
+
+        private void ToggleFilter_Key()
+        {
+            // Press Win + Ctrl + C
+            keybd_event(VK_LWIN, 0, 0, UIntPtr.Zero);
+            keybd_event(VK_CONTROL, 0, 0, UIntPtr.Zero);
+            keybd_event(VK_C, 0, 0, UIntPtr.Zero);
+
+            // Release in reverse order
+            keybd_event(VK_C, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+            keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+            keybd_event(VK_LWIN, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+        }
+
+        private void ToggleFilter_Click(object sender, EventArgs e)
+        {
+            if (isToggled == true)
+            {
+                isToggled = false;
+                ToggleFilter.Image = Properties.Resources.off;
+            } else
+            {
+                isToggled = true;
+                ToggleFilter.Image = Properties.Resources.on1;
+            }
+
+
+            ToggleFilter_Key();
+        }
+
+        private void reToggleFix()
+        {
+            string command = "Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\ColorFiltering\" -Name \"Active\" -Value 1";
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"{command}\"",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            using (Process process = Process.Start(psi))
+            {
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    MessageBox.Show("Error: " + error);
+                }
+            }
+        }
+
+        private async void Grayscale_Click(object sender, EventArgs e)
+        {
+            if (isGrayScale == false)
+            {
+                isGrayScale = true;
+                isProtan = false;
+
+                Protan.Image = Properties.Resources.off;
+                Grayscale.Image = Properties.Resources.on1;
+            } else
+            {
+                return;
+            }
+
+
+            string command = "Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\ColorFiltering\" -Name \"FilterType\" -Value 0";
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"{command}\"",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            using (Process process = Process.Start(psi))
+            {
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    MessageBox.Show("Error: " + error);
+                }
+            }
+
+            if (isToggled == true)
+            {
+                ToggleFilter_Key();
+                await Task.Delay(10);
+                ToggleFilter_Key();
+            }
+
+        }
+
+        private async void Protan_Click(object sender, EventArgs e)
+        {
+            if (isProtan == false)
+            {
+                isGrayScale = false;
+                isProtan = true;
+
+                Protan.Image = Properties.Resources.on1;
+                Grayscale.Image = Properties.Resources.off;
+            } else
+            {
+                return;
+            }
+
+
+            string command = "Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\ColorFiltering\" -Name \"FilterType\" -Value 4";
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"{command}\"",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            using (Process process = Process.Start(psi))
+            {
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    MessageBox.Show("Error: " + error);
+                }
+            }
+
+            if (isToggled == true)
+            {
+                ToggleFilter_Key();
+                await Task.Delay(10);
+                ToggleFilter_Key();
+            }
+
+
         }
     }
 }
